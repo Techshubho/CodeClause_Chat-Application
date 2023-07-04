dd61397d792817da2947e93c483ded77e1b91659
Suvam Sarkar <suvamsarkar.com@gmail.com>
2023-07-04 14:45:47 +0530

    Version 1

---
+++ helpers/formatDate.js
@@ -0,0 +1,14 @@
+
+// helper/formatDate.js
+
+const moment = require('moment');
+
+function formatMessage(username, text) {
+  return {
+    username,
+    text,
+    time: moment().format('h:mm a')
+  };
+}
+
+module.exports = formatMessage;
