b80ef887a4481e217c28d1b1c1b1b5ba2628a330
Suvam Sarkar <suvamsarkar.com@gmail.com>
2023-07-17 00:20:13 +0530

    Version 1

--- index.js
+++ index.js
@@ -15,41 +15,43 @@ const {
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-
-// Set public folder---------------------------------------------------------------------------------
 app.use(express.static(path.join(__dirname, 'public')));
 
-// this block will run when the client connects--------------------------------------------------------
+// this part will run when the user connects--------------------------------------------------------
 io.on('connection', socket => {
   socket.on('joinRoom', ({ username, room }) => {
     const user = newUser(socket.id, username, room);
 
     socket.join(user.room);
 
-// General welcome--------------------------------------------------------------------------------------
+// Grreting From WeMonk--------------------------------------------------------------------------------------
     socket.emit('message', formatMessage("WeMonk", 'Welcome, Monks! Your presence is valued for creating a peaceful environment.\n\nRegards,\nTeam WeMonk'));
 
-// Broadcast everytime users connects------------------------------------------------------------------
+// It will run everytime when users join WeMonk------------------------------------------------------------------
     socket.broadcast
       .to(user.room)
       .emit(
         'message',
         formatMessage("WeMonk", `${user.username} has joined the room`)
       );
-// Current active users and room name----------------------------------------------------------------------
+
+
+// Active users and room name----------------------------------------------------------------------
     io.to(user.room).emit('roomUsers', {
       room: user.room,
       users: getIndividualRoomUsers(user.room)
     });
   });
-// Listen for client message----------------------------------------------------------------------------------
+
+
+// Listen for user message----------------------------------------------------------------------------------
   socket.on('chatMessage', msg => {
     const user = getActiveUser(socket.id);
 
     io.to(user.room).emit('message', formatMessage(user.username, msg));
   });
 
-// Runs when client disconnects------------------------------------------------------------------------
+// Runs when user will disconnects------------------------------------------------------------------------
   socket.on('disconnect', () => {
     const user = exitRoom(socket.id);
 
@@ -58,9 +60,8 @@ io.on('connection', socket => {
         'message',
         formatMessage("WeMonk", `${user.username} has left the room`)
       );
-
-// Current active users and room name---------------------------------------------------------------------
-      io.to(user.room).emit('roomUsers', {
+      
+       io.to(user.room).emit('roomUsers', {
         room: user.room,
         users: getIndividualRoomUsers(user.room)
       });
